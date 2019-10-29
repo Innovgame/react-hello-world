f09b5ecfcb676594101c72e15846795187204835
lc <innovgame@163.com>
2019-10-29 23:40:43 +0800

    html structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import ListItems from './ListItems';
+import Header from './Header';
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
             todoItem: '',
-            items: []
+            items: ['吃香蕉', '吃苹果', '打篮球']
         }
     }
 
     render() {
         return (
-            <div>
-                
+            <div className="container">
+                <Header title="Todo List" />
+                <form className="form-wrap">
+                    <input value={this.state.todoItem} />
+                    <button>Submit</button>
+                </form>
+                <ListItems items={this.state.items} />
             </div>
         );
     }
